1736440240a1624453a4297a1621196f95d2d861
James Walker <James.Walker@Dal.ca>
2017-04-02 00:55:12 -0300

    Place functions in to a class structure
    The ChessBoard class was created to hold most of the previous functions
    and conform to a more object-oriented programming style.

--- src_Python/n_queens_counter.py
+++ src_Python/n_queens_counter.py
@@ -24,81 +24,76 @@ webpage: www.cs.utexas.edu/users/EWD/transcriptions/EWD03xx/EWD316.9.html
 from sys import argv
 
 
-class NQueensCounter(object):
-    def __init__(self):
-        """Initializes counters for tracking the number of times a queen is
-        placed on the chess board and the number of N-Queens solutions"""
+class ChessBoard(object):
+    def __init__(self, number_of_queens):
+        """Initializes starting values for the NxN chess board"""
+        self.n_size = number_of_queens
+        self.queen_positions = [0] * number_of_queens
+        self.column_j = 0
+        self.column = [True] * number_of_queens
+        diagonal_attack_lines_on_board = (2 * number_of_queens) - 1
+        self.diagonal_up = [True] * diagonal_attack_lines_on_board
+        self.diagonal_down = [True] * diagonal_attack_lines_on_board
         self.queen_placements = 0
         self.n_queens_solutions = 0
 
-    def update_placements(self):
-        """Increments the counter for the number of queens that have been
-        placed on the board"""
+    def index_of_diagonal_up_attack(self, row_i):
+        """Calculates the index for an upward diagonal attack move for the
+        given row in the current column"""
+        return (self.n_size - 1 + self.column_j - row_i)
+
+    def index_of_diagonal_down_attack(self, row_i):
+        """Calculates the index for an downward diagonal attack move for the
+        given row in the current column"""
+        return (self.column_j + row_i)
+
+    def square_is_free(self, row_i):
+        """Check if a queen can be placed on the chess board at the given row
+        in the current column"""
+        return (self.column[row_i] and
+                self.diagonal_up[self.index_of_diagonal_up_attack(row_i)] and
+                self.diagonal_down[self.index_of_diagonal_down_attack(row_i)])
+
+    def set_queen(self, row_i):
+        """Recursive function for finding valid queen placements on the chess
+        board. When the NxN board has N queens placed on it, the solutions
+        counter is incremented."""
+        self.queen_positions[self.column_j] = row_i
+        self.column[row_i] = False
+        self.diagonal_up[self.index_of_diagonal_up_attack(row_i)] = False
+        self.diagonal_down[self.index_of_diagonal_down_attack(row_i)] = False
+        self.column_j += 1
         self.queen_placements += 1
 
-    def update_solution_count(self):
-        """Increments the counter for the number of N-Queens solutions found"""
-        self.n_queens_solutions += 1
+    def remove_queen(self, row_i):
+        """Removes a queen from the chess board at the given row in the
+        current column"""
+        self.column_j -= 1
+        self.column[row_i] = True
+        self.diagonal_up[self.index_of_diagonal_up_attack(row_i)] = True
+        self.diagonal_down[self.index_of_diagonal_down_attack(row_i)] = True
 
+    def place_next_queen(self):
+        """Recursive function for finding valid queen placements on the chess
+        board. When the NxN board has N queens placed on it, the solutions
+        counter is incremented."""
+        for row_i in range(0, self.n_size):
+            if self.square_is_free(row_i):
+                self.set_queen(row_i)
+                if self.column_j == self.n_size:
+                    self.n_queens_solutions += 1
+                else:
+                    self.place_next_queen()
+                self.remove_queen(row_i)
 
-def initialize_board(number_of_queens):
-    """Initializes starting values for the NxN chess board"""
-    queen_positions = [0] * number_of_queens
-    column_j = 0
-    column = [True] * number_of_queens
-    diagonal_up = [True] * ((2 * number_of_queens) - 1)
-    diagonal_down = [True] * ((2 * number_of_queens) - 1)
-    chess_board = [
-        queen_positions,
-        column_j, column,
-        diagonal_up,
-        diagonal_down
-    ]
-    return (chess_board)
-
-
-def square_is_free(chess_board, number_of_queens, row_i):
-    """Check if a queen can be placed on the current square"""
-    diagonal_up_index = number_of_queens - 1 + chess_board[1] - row_i
-    diagonal_down_index = chess_board[1] + row_i
-    return (chess_board[2][row_i] and chess_board[3][diagonal_up_index] and
-            chess_board[4][diagonal_down_index])
-
-
-def set_queen(chess_board, number_of_queens, row_i, counter):
-    """Places a queen on the chess board in the given row"""
-    diagonal_up_index = number_of_queens - 1 + chess_board[1] - row_i
-    diagonal_down_index = chess_board[1] + row_i
-    chess_board[0][chess_board[1]] = row_i + 1
-    chess_board[2][row_i] = False
-    chess_board[3][diagonal_up_index] = False
-    chess_board[4][diagonal_down_index] = False
-    chess_board[1] += 1
-    counter.update_placements()
-    return (chess_board)
-
-
-def remove_queen(chess_board, number_of_queens, row_i):
-    """Removes a queen from the chess board in the current column"""
-    chess_board[1] -= 1
-    chess_board[2][row_i] = True
-    chess_board[3][number_of_queens - 1 + chess_board[1] - row_i] = True
-    chess_board[4][chess_board[1] + row_i] = True
-    return (chess_board)
-
-
-def place_next_queen(chess_board, number_of_queens, counter):
-    """Recursive function for finding valid queen placements on the chess
-    board"""
-    for row_i in range(0, number_of_queens):
-        if (square_is_free(chess_board, number_of_queens, row_i)):
-            chess_board = set_queen(chess_board, number_of_queens, row_i,
-                                    counter)
-            if (chess_board[1] == number_of_queens):
-                counter.update_solution_count()
-            else:
-                place_next_queen(chess_board, number_of_queens, counter)
-            chess_board = remove_queen(chess_board, number_of_queens, row_i)
+    def output_count_of_n_queens_solutions(self):
+        """Starts the N-Queens solution finding algorithm and prints the final
+        result"""
+        self.place_next_queen()
+        result_string = "The %d-Queens problem required %d queen placements " \
+                        "to find all %d solutions"
+        print (result_string) % (self.n_size, self.queen_placements,
+                                 self.n_queens_solutions)
 
 
 def main(argv):
@@ -110,13 +105,7 @@ def main(argv):
     number_of_queens = 4
     if len(argv) != 0 and int(argv[0]) > 0:
         number_of_queens = int(argv[0])
-    counter = NQueensCounter()
-    chess_board = initialize_board(number_of_queens)
-    place_next_queen(chess_board, number_of_queens, counter)
-    result_string = "The %d-Queens problem required %d queen placements to " \
-                    "find all %d solutions"
-    print (result_string) % (number_of_queens, counter.queen_placements,
-                             counter.n_queens_solutions)
+    ChessBoard(number_of_queens).output_count_of_n_queens_solutions()
 
 
 if __name__ == '__main__':
