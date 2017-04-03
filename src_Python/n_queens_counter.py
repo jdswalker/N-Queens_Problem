ba3471df0576afc1430e02e53472a91397ef8c75
James Walker <James.Walker@Dal.ca>
2017-04-03 12:25:34 -0300

    Add symmtry constraint to reduce search space
    The symmetry of the board allows the number of solutions to be
    calculated using approximately half of the queen placements of the
    non-symmetry constrained DFS algorithm.

--- src_Python/n_queens_counter.py
+++ src_Python/n_queens_counter.py
@@ -13,7 +13,7 @@ Purpose:
   as the number of times a queen is placed during the program's execution.
 Example Usage:
   $ python ./n_queens_counter.py 12
-  The 12-Queens problem required 856188 queen placements to find all 14200
+  The 12-Queens problem required 428094 queen placements to find all 14200
   solutions
 
 This implementation is based off the algorithm provided at the bottom of this
@@ -56,8 +56,7 @@ class ChessBoard(object):
 
     def set_queen(self, row_i):
         """Places a queen on the chess board at the given row in the current
-        column. Each time a queen is placed, the counter for queen placements
-        is incremented"""
+        column"""
         self.queen_positions[self.column_j] = row_i
         self.column[row_i] = False
         self.diagonal_up[self.index_of_diagonal_up_attack(row_i)] = False
@@ -86,17 +85,28 @@ class ChessBoard(object):
                     self.place_next_queen()
                 self.remove_queen(row_i)
 
-    def count_n_queens_solutions_from_position(self, row_to_place_first_queen):
-        """Counts all of the valid N-queens solutions when the first queen
-        placement is fixed (i.e., starting from a given row in the first
-        column)"""
-        self.set_queen(row_to_place_first_queen)
-        self.place_next_queen()
-        result_string = "When the first queen is placed in row %d:\n   " \
-                        "%d queens were placed and %d solutions were found"
-        print (result_string) % (row_to_place_first_queen + 1,
-                                 self.n_queens_solutions,
-                                 self.queen_placements)
+
+def get_counts_for_n_queens_solutions(number_of_queens):
+    """Calculates the number of queens that are placed on the chessboard while
+    finding all of the valid solutions for a given number of queens. The number
+    of queen placements and solutions is then returned."""
+    total_queen_placements = 0
+    total_n_queens_solutions = 0
+    # The next two variables are used to divide the search-space of the chess
+    # board in half by taking advantage of the symmetry in the chess board
+    number_of_column_pairs = number_of_queens // 2
+    middle_row = number_of_column_pairs + (number_of_queens % 2)
+    for row_i in range(0, middle_row):
+        chess_board = ChessBoard(number_of_queens)
+        chess_board.set_queen(row_i)
+        chess_board.place_next_queen()
+        total_queen_placements += chess_board.queen_placements
+        total_n_queens_solutions += chess_board.n_queens_solutions
+        # The if statement below will only be false when row_i is the index of
+        # the middle row for a chess board with an odd number of rows
+        if row_i != number_of_column_pairs:
+            total_n_queens_solutions += chess_board.n_queens_solutions
+    return (total_queen_placements, total_n_queens_solutions)
 
 
 def main(argv):
@@ -108,19 +118,12 @@ def main(argv):
     number_of_queens = 4
     if len(argv) != 0 and int(argv[0]) > 0:
         number_of_queens = int(argv[0])
-    total_queen_placements = 0
-    total_n_queens_solutions = 0
-    # This loop partitions the calculations by the row where the first queen
-    # is placed in the first column
-    for row_i in range(0, number_of_queens):
-        chess_board = ChessBoard(number_of_queens)
-        chess_board.count_n_queens_solutions_from_position(row_i)
-        total_queen_placements += chess_board.queen_placements
-        total_n_queens_solutions += chess_board.n_queens_solutions
+    queen_placements, n_queens_solutions = \
+        get_counts_for_n_queens_solutions(number_of_queens)
     result_string = "\nThe %d-Queens problem required %d queen placements " \
                     "to find all %d solutions"
-    print (result_string) % (number_of_queens, total_queen_placements,
-                             total_n_queens_solutions)
+    print (result_string) % (number_of_queens, queen_placements,
+                             n_queens_solutions)
 
 
 if __name__ == '__main__':
