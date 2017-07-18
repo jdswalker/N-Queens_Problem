076d6f35e89ca16709e6dbcafc44aaf8563e16da
James Walker <James.Walker@Dal.ca>
2017-07-18 01:15:38 -0300

    Refactor dynamic memory allocation
    Like the counter program, the dynamic memory allocation was simplified
    by allocating all of the position arrays at the same time. Function
    descriptions were also added.

--- src_C/n_queens_solver.c
+++ src_C/n_queens_solver.c
@@ -7,138 +7,112 @@
 //   The N-Queens Solver finds solutions for the N-Queens problem. That is, how
 //   many ways are there to place N chess queens on an NxN chess board such
 //   that none of the queens can attack each other.
-// Compilation, Execution and Partial Output:
+// Compilation, Execution, and Sample Output:
 //   $ gcc -std=c99 n_queens_solver.c -o n_queens_solver
-//   $ ./n_queens_solver.exe 8
-//   1 5 8 6 3 7 2 4
-//   1 6 8 3 7 4 2 5
-//   ...
-//   8 3 1 6 2 5 7 4
-//   8 4 1 3 6 2 7 5
+//   $ ./n_queens_solver.exe 6
+//   2 4 6 1 3 5
+//   3 6 2 5 1 4
+//   4 1 5 2 6 3
+//   5 3 1 6 4 2
 //
 // This implementation is based off the algorithm provided at the bottom of this
 // webpage: www.cs.utexas.edu/users/EWD/transcriptions/EWD03xx/EWD316.9.html
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct chess_board {
   // Chess board variables
-  int n_size;         // Number of queens on the NxN chess board
-  int *queens;        // Store queen positions on the board
-  int *column;        // Store available column moves/attacks
-  int *diagonal_up;   // Store available diagonal moves/attacks
-  int *diagonal_down;
-  int column_j;          // Store current column to examine on the board
+  uint32_t n_size;            // Number of queens on the NxN chess board
+  uint32_t *queen_positions;  // Store queen positions on the board
+  uint32_t *column;           // Store available column moves/attacks
+  uint32_t *diagonal_up;      // Store available diagonal moves/attacks
+  uint32_t *diagonal_down;
+  uint32_t column_j;          // Store current column to examine on
 };
 
-void allocation_error(const int error_code) {
-  switch (error_code) {
-    case 0:
-      fprintf(stderr, "The number of queens must be greater than 0.\n");
-    case 1:
-      fprintf(stderr, "Failed to allocate memory for chess board.\n");
-      break;
-    case 2:
-      fprintf(stderr, "Failed to allocate memory for chess queens.\n");
-      break;
-    case 3:
-      fprintf(stderr, "Failed to allocate memory for column attacks.\n");
-      break;
-    case 4:
-      fprintf(stderr, "Failed to allocate memory for diagonal up attacks.\n");
-      break;
-    case 5:
-      fprintf(stderr, "Failed to allocate memory for diagonal down attacks.\n");
-      break;
-  }
-  exit(EXIT_FAILURE);
-}
-
-struct chess_board *initialize_board(const int n_queens) {
+struct chess_board *initialize_board(const uint32_t n_queens) {
   if (n_queens < 1) {
-    allocation_error(0);
+    fprintf(stderr, "The number of queens must be greater than 0.\n");
+    exit(EXIT_FAILURE);
   }
+
   // Dynamically allocate memory for chessboard
   struct chess_board *board = malloc(sizeof(struct chess_board));
   if (board == NULL) {
-    allocation_error(1);
+    fprintf(stderr, "Memory allocation failed for chess board.\n");
+    exit(EXIT_FAILURE);
   }
-  // Dynamically allocate memory for chessboard variables
-  board->queens = (int *)malloc(sizeof(int) * n_queens);
-  if(board->queens == NULL) {
-    allocation_error(2);
+
+  // Dynamically allocate memory for chessboard arrays that track positions
+  const uint32_t diagonal_size = 2 * n_queens - 1;
+  const uint32_t total_size = 2 * (n_queens + diagonal_size);
+  board->queen_positions = malloc(sizeof(uint32_t) * total_size);
+  if(board->queen_positions == NULL) {
+    fprintf(stderr, "Memory allocation failed for the chess board arrays.\n");
+    exit(EXIT_FAILURE);
   }
-  board->column = (int *)malloc(sizeof(int) * n_queens);
-  if(board->column == NULL) {
-    allocation_error(3);
-  }
-  board->diagonal_up = (int *)malloc(sizeof(int) * (2*n_queens - 1));
-  if(board->diagonal_up == NULL) {
-    allocation_error(4);
-  }
-  board->diagonal_down = (int *)malloc(sizeof(int) * (2*n_queens - 1));
-  if(board->diagonal_down == NULL) {
-    allocation_error(5);
-  }
+  board->column = &board->queen_positions[n_queens];
+  board->diagonal_up = &board->column[n_queens];
+  board->diagonal_down = &board->diagonal_up[diagonal_size];
+
   // Initialize the chess board variables
   board->n_size = n_queens;
+  for(uint32_t i = 0; i < n_queens; ++i) {
+    board->queen_positions[i] = 0;
+  }
+  for(uint32_t i = n_queens; i < total_size; ++i) {
+    // Initializes values for column, diagonal_up, and diagonal_down
+    board->queen_positions[i] = 1;
+  }
   board->column_j = 0;
-  for(int i = 0; i < n_queens; ++i) {
-    board->queens[i] = 0;
-    board->column[i] = 1;
-    board->diagonal_up[i] = 1;
-    board->diagonal_down[i] = 1;
-  }
-  // Initialize remaining array indices
-  for(int i = n_queens; i < (2*n_queens - 1); ++i) {
-    board->diagonal_up[i] = 1;
-    board->diagonal_down[i] = 1;
-  }
+
   return board;
 }
 
+// Frees the dynamically allocated memory for the chess board structure
 void smash_board(struct chess_board *board) {
-  free(board->queens);
-  free(board->column);
-  free(board->diagonal_up);
-  free(board->diagonal_down);
+  free(board->queen_positions);
   free(board);
 }
 
 // Check if a queen can be placed in column 'i', at row 'j'
-int square_is_free(const struct chess_board *board, const int row_i) {
+int square_is_free(const struct chess_board *board, const uint32_t row_i) {
   return board->column[row_i] &
          board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] &
-         board->diagonal_down[(board->column_j + row_i)];
+         board->diagonal_down[board->column_j + row_i];
 }
 
 // Place a queen on the chess board
-void set_queen(struct chess_board *board, const int row_i) {
-  board->queens[board->column_j] = row_i;
+void set_queen(struct chess_board *board, const uint32_t row_i) {
+  board->queen_positions[board->column_j] = row_i;
   board->column[row_i] = 0;
   board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] = 0;
-  board->diagonal_down[(board->column_j + row_i)] = 0;
+  board->diagonal_down[board->column_j + row_i] = 0;
   board->column_j += 1;
 }
 
-void remove_queen(struct chess_board *board, const int row_i) {
+// Removes a queen from the NxN chess board in the given column to backtrack
+void remove_queen(struct chess_board *board, const uint32_t row_i) {
   board->column_j -= 1;
   board->diagonal_down[(board->column_j + row_i)] = 1;
   board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] = 1;
   board->column[row_i] = 1;
 }
 
+// Prints an N-Queens solution using row indices to indicate the position of
+// each queen on the chess board
 void print_solution(const struct chess_board *board) {
-  for (int col = 0; col < board->n_size; ++col) {
-    fprintf(stdout, "%d ", board->queens[col] + 1);
+  for (uint32_t column = 0; column < board->n_size; ++column) {
+    fprintf(stdout, "%d ", board->queen_positions[column] + 1);
   }
   fputc('\n', stdout);
 }
 
 void place_next_queen(struct chess_board *board) {
-  for (int row_i = 0; row_i < board->n_size; ++row_i) {
+  for (uint32_t row_i = 0; row_i < board->n_size; ++row_i) {
     if (square_is_free(board, row_i)) {
       set_queen(board, row_i);
       if (board->column_j == board->n_size) {
@@ -163,9 +137,9 @@ int main(int argc, char *argv[]) {
 	} else {
     board = initialize_board(atoi(argv[1]));
   }
-  // Start solver algorithm
-  place_next_queen(board);
-  // Free dynamically allocated memory
-  smash_board(board);
+
+  place_next_queen(board);  // Start solver algorithm
+  smash_board(board);  // Free dynamically allocated memory
+
   return EXIT_SUCCESS;
 }
