d52f9d2a3140c2f33e61fc743acca739ceba86c0
James Walker <James.Walker@Dal.ca>
2017-07-19 00:17:15 -0300

    Update algorithm to contrain board symmetry
    A slightly improved version of the symmetry contrained algorithm used in
    the Python programs was implemented for the C Counter. The C program
    optimizes searching with odd-numbered N-Queens more than the Python
    programs, and also includes broader use of const and static keywords.

--- src_C/n_queens_counter.c
+++ src_C/n_queens_counter.c
@@ -4,45 +4,48 @@
 //   http://www.opensource.org/licenses/mit-license.php
 //
 // Purpose: 
-//   The N-Queens Counter finds follows the same algorithm as the N-Queens 
-//   Solver, except it does not return any of the solutions. Instead, the 
-//   program counts the number of solutions for a given N-Queens problem as 
-//   well as the number of times a queen is placed during the program's
-//   execution.
+//   The N-Queens Counter finds follows an improved version of the algorithm
+//   used by the N-Queens Solver, except it does not return any of the
+//   solutions. Instead, the program counts the number of solutions for a given
+//   N-Queens problem as well as the number of times a queen is placed during
+//   the program's execution.
 // Compilation, Execution, and Partial Output:
 //   $ gcc -std=c99 -O2 n_queens_counter.c -o n_queens_counter
 //   $ ./n_queens_counter.exe 12
-//   The 12-Queens problem required 856188 queen placements to find all 14200
+//   The 12-Queens problem required 428094 queen placements to find all 14200
 //   solutions
 //
-// This implementation is based off the algorithm provided at the bottom of this
-// webpage: www.cs.utexas.edu/users/EWD/transcriptions/EWD03xx/EWD316.9.html
+// This implementation adapted from the algorithm provided at the bottom of
+// this webpage:
+//   www.cs.utexas.edu/users/EWD/transcriptions/EWD03xx/EWD316.9.html
 
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// An abstract representation of an NxN chess board to tracking open positions
 struct chess_board {
-  // Chess board variables
   uint32_t n_size;            // Number of queens on the NxN chess board
   uint32_t *queen_positions;  // Store queen positions on the board
   uint32_t *column;           // Store available column moves/attacks
   uint32_t *diagonal_up;      // Store available diagonal moves/attacks
   uint32_t *diagonal_down;
-  uint32_t column_j;          // Store current column to examine on the board
+  uint32_t column_j;          // Stores column to place the next queen in
   uint64_t placements;        // Tracks total number queen placements
   uint32_t solutions;         // Tracks number of solutions
 };
+static struct chess_board *board;
 
-struct chess_board *initialize_board(const uint32_t n_queens) {
+// Handles dynamic memory allocation of the arrays and sets initial values
+static void initialize_board(const uint32_t n_queens) {
   if (n_queens < 1) {
     fprintf(stderr, "The number of queens must be greater than 0.\n");
     exit(EXIT_FAILURE);
   }
 
-  // Dynamically allocate memory for chessboard
-  struct chess_board *board = malloc(sizeof(struct chess_board));
+  // Dynamically allocate memory for chessboard struct
+  board = malloc(sizeof(struct chess_board));
   if (board == NULL) {
     fprintf(stderr, "Memory allocation failed for chess board.\n");
     exit(EXIT_FAILURE);
@@ -60,7 +63,7 @@ struct chess_board *initialize_board(const uint32_t n_queens) {
   board->diagonal_up = &board->column[n_queens];
   board->diagonal_down = &board->diagonal_up[diagonal_size];
 
-  // Initialize the chess board variables
+  // Initialize the chess board parameters
   board->n_size = n_queens;
   for(uint32_t i = 0; i < n_queens; ++i) {
     board->queen_positions[i] = 0;
@@ -72,25 +75,23 @@ struct chess_board *initialize_board(const uint32_t n_queens) {
   board->column_j = 0;
   board->placements = 0;
   board->solutions = 0;
-
-  return board;
 }
 
 // Frees the dynamically allocated memory for the chess board structure
-void smash_board(struct chess_board *board) {
+static void smash_board() {
   free(board->queen_positions);
   free(board);
 }
 
-// Check if a queen can be placed in column 'i', at row 'j'
-int square_is_free(const struct chess_board *board, const uint32_t row_i) {
+// Check if a queen can be placed in at row 'i' of the current column
+static uint32_t square_is_free(const uint32_t row_i) {
   return board->column[row_i] &
          board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] &
          board->diagonal_down[board->column_j + row_i];
 }
 
-// Place a queen on the chess board
-void set_queen(struct chess_board *board, const uint32_t row_i) {
+// Place a queen on the chess board at row 'i' of the current column
+static void set_queen(const uint32_t row_i) {
   board->queen_positions[board->column_j] = row_i;
   board->column[row_i] = 0;
   board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] = 0;
@@ -100,7 +101,7 @@ void set_queen(struct chess_board *board, const uint32_t row_i) {
 }
 
 // Removes a queen from the NxN chess board in the given column to backtrack
-void remove_queen(struct chess_board *board, const uint32_t row_i) {
+static void remove_queen(const uint32_t row_i) {
   --board->column_j;
   board->diagonal_down[board->column_j + row_i] = 1;
   board->diagonal_up[(board->n_size - 1) + (board->column_j - row_i)] = 1;
@@ -108,40 +109,49 @@ void remove_queen(struct chess_board *board, const uint32_t row_i) {
 }
 
 // Prints the number of queen placements and solutions for the NxN chess board
-void print_counts(const struct chess_board *board) {
-  const char *output = "The %d-Queens problem required "
-                       "%"PRIu64" queen placements to find all %d solutions\n";
-  fprintf(stdout, output, board->n_size, board->placements, board->solutions);
+static void print_counts() {
+  // The next line fixes double-counting when solving the 1-queen problem
+  const uint32_t solution_count = board->n_size == 1 ? 1 : board->solutions;
+  const char const output[] = "The %u-Queens problem required %lu queen "
+                              "placements to find all %u solutions\n";
+  fprintf(stdout, output, board->n_size, board->placements, solution_count);
 }
 
 // Recursive function for finding valid queen placements on the chess board
-void place_next_queen(struct chess_board *board) {
-  for (uint32_t row_i = 0; row_i < board->n_size; ++row_i) {
-    if (square_is_free(board, row_i)) {
-      set_queen(board, row_i);
+static void place_next_queen(const uint32_t row_boundary) {
+  const uint32_t middle = board->column_j ? board->n_size : board->n_size >> 1;
+  for (uint32_t row_i = 0; row_i < row_boundary; ++row_i) {
+    if (square_is_free(row_i)) {
+      set_queen(row_i);
       if (board->column_j == board->n_size) {
-        // Chess board has NxN queens set
-        ++board->solutions;
+        // Due to 2-fold symmetry of the chess board, accurate counts can be
+        // obtained by only searching half the board and double-counting each
+        // solution found (the sole exception being the 1-Queen problem)
+        board->solutions += 2;
+      } else if (board->queen_positions[0] != middle) {
+        place_next_queen(board->n_size);
       } else {
-        place_next_queen(board);
+        // This branch should only execute once at most, and only for odd
+        // numbered N-Queens problems
+        place_next_queen(middle);
       }
-      remove_queen(board, row_i);
+      remove_queen(row_i);
     }
   }
 }
 
 int main(int argc, char *argv[]) {
-  struct chess_board *board;
-	if (argc == 1) {
-	  // Defaults to the 4-queens problem if no input is provided
-    board = initialize_board(4);
-	} else {
-    board = initialize_board(atoi(argv[1]));
-  }
+  static const uint32_t default_n = 4;
+  const uint32_t n_queens = (argc != 1) ? (uint32_t)atoi(argv[1]) : default_n;
+  initialize_board(n_queens);
 
-  place_next_queen(board);  // Start solver algorithm
-  print_counts(board);
-  smash_board(board);  // Free dynamically allocated memory
+  // Determines the index for the middle row to take advantage of board
+  // symmetry when searching for solutions
+  const uint32_t row_boundary = (n_queens >> 1) + (n_queens & 1);
+
+  place_next_queen(row_boundary);  // Start solver algorithm
+  print_counts();
+  smash_board();  // Free dynamically allocated memory
 
   return EXIT_SUCCESS;
 }
