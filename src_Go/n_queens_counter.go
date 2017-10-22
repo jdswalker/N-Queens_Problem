0316bef6d5984118ea21187884f3729b513d532e
U-JDSW\JDSW <James Walker>
2017-10-22 19:06:07 -0300

    Folded slices into a struct

--- src_Go/n_queens_counter.go
+++ src_Go/n_queens_counter.go
@@ -4,27 +4,57 @@ import "fmt"
 import "os"
 import "strconv"
 
-func place_next_queen(columns []bool, diagonal_up []bool, diagonal_down []bool, column_j int, placements *int, solutions *int) {
-	for row_i := 0; row_i < len(columns); row_i++ {
-		if columns[row_i] && diagonal_up[len(columns)-1+column_j-row_i] && diagonal_down[column_j+row_i] {
-			columns[row_i] = false
-			diagonal_up[len(columns)-1+column_j-row_i] = false
-			diagonal_down[column_j+row_i] = false
-			column_j++
-			*placements++
+type ChessBoard struct {
+	columns []bool        // Store available column moves/attacks
+	diagonal_up []bool    // Store available diagonal moves/attacks
+	diagonal_down []bool
+	column_j int          // Stores column to place the next queen in
+	placements uint64     // Tracks total number queen placements
+	solutions uint64      // Tracks number of solutions
+}
 
-			if column_j == len(columns) {
+func initialize_board(n_size int) *ChessBoard {
+	diagonal_size := 2*n_size - 1
+	board := &ChessBoard{
+		columns: make([]bool, n_size, n_size),
+		diagonal_up: make([]bool, diagonal_size, diagonal_size),
+		diagonal_down: make([]bool, diagonal_size, diagonal_size),
+		column_j: 0,
+		placements: 0,
+		solutions: 0,
+	}
+	for i := 0; i < n_size; i++ {
+		board.columns[i] = true
+	}
+	for i := 0; i < (2*n_size - 1); i++ {
+		board.diagonal_up[i] = true
+	}
+	copy(board.diagonal_down, board.diagonal_up)
+
+	return board
+}
+
+func place_next_queen(board *ChessBoard) {
+	for row_i := 0; row_i < len(board.columns); row_i++ {
+		if board.columns[row_i] && board.diagonal_up[len(board.columns)-1+board.column_j-row_i] && board.diagonal_down[board.column_j+row_i] {
+			board.columns[row_i] = false
+			board.diagonal_up[len(board.columns)-1+board.column_j-row_i] = false
+			board.diagonal_down[board.column_j+row_i] = false
+			board.column_j++
+			board.placements++
+
+			if board.column_j == len(board.columns) {
 				// Chess board is full
-				*solutions++
+				board.solutions++
 			} else {
 				// Recursive call to find next queen placement on the chess board
-				place_next_queen(columns, diagonal_up, diagonal_down, column_j, placements, solutions)
+				place_next_queen(board)
 			}
 			// Removes a queen from the chess board in the given column to backtrack
-			column_j--
-			diagonal_down[column_j+row_i] = true
-			diagonal_up[len(columns)-1+column_j-row_i] = true
-			columns[row_i] = true
+			board.column_j--
+			board.diagonal_down[board.column_j+row_i] = true
+			board.diagonal_up[len(board.columns)-1+board.column_j-row_i] = true
+			board.columns[row_i] = true
 		}
 	}
 }
@@ -37,26 +67,8 @@ func main() {
 			n_size = user_input
 		}
 	}
-
-	diagonal_size := 2*n_size + 1
-    // Attacks along a column
-	columns := make([]bool, n_size, n_size)
-    // Attacks along diagonals
-	diagonal_up := make([]bool, diagonal_size, diagonal_size)
-	diagonal_down := make([]bool, diagonal_size, diagonal_size)
-	column_j := 0
-	placements := 0
-	solutions := 0
-
-	for i := 0; i < n_size; i++ {
-		columns[i] = true
-	}
-	for i := 0; i < (2*n_size + 1); i++ {
-		diagonal_up[i] = true
-	}
-	copy(diagonal_down, diagonal_up)
-
-	place_next_queen(columns, diagonal_up, diagonal_down, column_j, &placements, &solutions)
+	board := initialize_board(n_size)
+	place_next_queen(board)
 	const template string = "The %d-Queens problem required %d queen placements to find all %d solutions"
-	fmt.Printf(template, n_size, placements, solutions)
+	fmt.Printf(template, n_size, board.placements, board.solutions)
 }
